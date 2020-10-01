731e4e5f06ab80697fafbd1575c7cad30a04e483
Autonomousse <ucsddatacamp@gmail.com>
2020-10-01 02:15:59 -0700

    Created working map with different colors, added popups

--- static/js/logic.js
+++ static/js/logic.js
@@ -39,105 +39,114 @@ var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}
   accessToken: API_KEY
 });
 
-// // Initialize earthquake and tectonic plate layers
-// var layers = {
-//   earthquakes: new L.LayerGroup(),
-//   tectonic: new L.LayerGroup()
-// };
-
-// // Create the map with layers, at the mapid ID in the HTML file
-// var map = L.map("mapid", {
-//   center: [39.8283, -98.5795], //center of the US
-//   zoom: 5,
-//   layers: [
-//     layers.earthquakes,
-//     layers.tectonic
-//   ]
-// });
-
-// // Add the base light map tile layer to the map
-// light.addTo(map);
-
-// // Create the map objects to add to the layer control
-// var baseMaps = {
-//   "Light": light,
-//   "Dark": dark,
-//   "Outdoors": outdoor,
-//   "Satellite": satellite
-// };
-
-// // Create the overlays object to add to the layer control
-// var overlays = {
-//   "Earthquakes": layers.earthquakes,
-//   "Tectonic Plates": layers.tectonic
-// }
-
-// // Create a control for the layers and add the layers to it
-// L.control.layers(baseMaps, overlays).addTo(map);
-
-// The link to grab the geoJSON data from
-var geoJSON = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojson";
-
-var eq_data;
+// The link to grab the geoJSON data from USGS
+var geoJSON = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_month.geojson";
 
 // Create a promise and perform an API call for USGS past week M2.5+ earthquakes
 d3.json(geoJSON).then(function (geoData) {
 
   // Once the data loads, send the features object to the featureIterate function
   featureIterate(geoData.features);
+
 }).catch(function (error) {
   console.log(error);
 });
-  // Create arrays to hold the circle markers
-  var eq_locations = [];
 
-  function featureIterate(data) {
+// Create arrays to hold the circle markers
+var eq_locations = [];
 
-    //console.log(geoData.length)
-    // Create a function that will iterate through the data and create circles based on the magnitude
-    for (var x = 0; x < data.length; x++) {
-      eq_locations.push(
-        L.circle(data[x].geometry.coordinates.slice(0, 2).reverse(), {
-          stroke: false,
-          fillOpacity: 1,
-          color: "black",
-          fillColor: "black",
-          radius: data[x].properties.mag*10000
-        })
-      );
-    }
-    // Create two separate layer groups: one for earthquakes and one for tectonic plates
-    var eq = L.layerGroup(eq_locations);
-    console.log(eq_locations)
-    // Create the map with layers, at the mapid ID in the HTML file
-    var map = L.map("mapid", {
-      center: [39.8283, -98.5795], //center of the US
-      zoom: 5,
-      layers: [
-        light, eq
-      ]
-    });
+function featureIterate(data) {
 
-    // Add the base light map tile layer to the map
-    //light.addTo(map);
-
-    // Create the map objects to add to the layer control
-    var baseMaps = {
-      "Light": light,
-      "Dark": dark,
-      "Outdoors": outdoor,
-      "Satellite": satellite
-    };
-
-    // Create the overlays object to add to the layer control
-    var overlayMaps = {
-      "Earthquakes": eq
-    }
-
-    // Create a control for the layers and add the layers to it
-    L.control.layers(baseMaps, overlayMaps).addTo(map);
+  // Create a function that will iterate through the data and create circles based on the magnitude
+  // The color of the circle will be retrieved from the circleColor function
+  // Bind some popups giving information about the earthquake
+  for (var x = 0; x < data.length; x++) {
+    eq_locations.push(
+      L.circle(data[x].geometry.coordinates.slice(0, 2).reverse(), {
+        stroke: false,
+        fillOpacity: 0.75,
+        fillColor: circleColor(data[x].geometry.coordinates.slice(2, 3)),
+        color: "black",
+        radius: data[x].properties.mag * 10000
+      }).bindPopup("<h3> Location: <em>" + data[x].properties.place + "</em> <hr> Magnitude: <em>" + data[x].properties.mag + "</em> <hr> Date: <em>" + dateConversion(data[x].properties.time) + "</em> </h3>")
+    );
   }
 
+  // Create two separate layer groups: one for earthquakes and one for tectonic plates
+  var eq = L.layerGroup(eq_locations);
+  var tectonic = L.layerGroup();
+
+  // Create the map with layers, at the mapid ID in the HTML file
+  var map = L.map("mapid", {
+    center: [39.8283, -98.5795], //center of the US
+    zoom: 4,
+    layers: [
+      light, eq
+    ]
+  });
+
+  // Create the map objects to add to the layer control
+  var baseMaps = {
+    "Light": light,
+    "Dark": dark,
+    "Outdoors": outdoor,
+    "Satellite": satellite
+  };
+
+  // Create the overlays object to add to the layer control
+  var overlayMaps = {
+    "Earthquakes": eq
+  }
+
+  // Create a control for the layers and add the layers to it
+  L.control.layers(baseMaps, overlayMaps).addTo(map);
+}
+
+// Function that returns the color value for different depths
+function circleColor(depth) {
+
+  var color = "";
+
+  if (depth > 90) {
+    color = "#e55343";
+  }
+  else if (depth > 80) {
+    color = "#e27243";
+  }
+  else if (depth > 70) {
+    color = "#df9144";
+  }
+  else if (depth > 60) {
+    color = "#ddae44";
+  }
+  else if (depth > 50) {
+    color = "#daca45";
+  }
+  else if (depth > 40) {
+    color = "#cad745";
+  }
+  else if (depth > 30) {
+    color = "#abd545";
+  }
+  else if (depth > 20) {
+    color = "#8ed246";
+  }
+  else if (depth > 10) {
+    color = "#72cf46";
+  }
+  else {
+    color = "#56cd46";
+  }
+
+  return color;
+};
+
+// Function that takes the Unix date value and returns a more human legible value
+function dateConversion(unix) {
+  var dateObject = new Date(unix)
+  var converted = dateObject.toLocaleString();
+  return converted;
+}
 
 // }).catch(function (error) {
 //   console.log(error);
